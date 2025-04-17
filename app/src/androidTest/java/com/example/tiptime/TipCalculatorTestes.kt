1251a350484f3bef62ec5df85c485c25860ac624
Caie Hallite <hallitecaie@gmail.com>
2025-04-17 18:03:51 -0300

    3th Commit -> test ready to run

--- app/src/androidTest/java/com/example/tiptime/TipCalculatorTestes.kt
+++ app/src/androidTest/java/com/example/tiptime/TipCalculatorTestes.kt
@@ -1,8 +1,18 @@
 package com.example.tiptime
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import java.text.NumberFormat
 
 class TipCalculatorTestes {
 
-    //@Test
+    @Test
+    fun calculateTip_20PercentNoRoundUp(){
+        val amout = 10.00
+        val tip = 20.00
+
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amout, tip, false)
+        assertEquals(expectedTip, actualTip)
+    }
 }
